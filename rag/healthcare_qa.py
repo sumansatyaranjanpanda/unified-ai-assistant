ec9aca30011773847e87a2242a3d80446cde30b7
Raja Panda <satyaranjanpandasuman@gmail.com>
2025-08-14 20:28:54 +0530

    updated requirements.txt

--- rag/healthcare_qa.py
+++ rag/healthcare_qa.py
@@ -6,10 +6,13 @@ from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 
 load_dotenv()
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+##os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
-llm=ChatOpenAI(
-    model_name="gpt-3.5-turbo",temperature=0)
+llm = ChatOpenAI(
+    model="gpt-4o-mini",  # or "gpt-4o", "gpt-3.5-turbo"
+    temperature=0,
+    api_key=os.getenv("OPENAI_API_KEY")  # or st.secrets["OPENAI_API_KEY"]
+)
 
 # Compute project root (one level up from this file)
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
